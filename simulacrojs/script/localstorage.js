ba77f4fc10bb582002a238db2de0acd00a575b98
Nesdael <danieldurfue7@gmail.com>
2026-06-02 20:33:45 -0500

    cons servidores

---
+++ simulacrojs/script/localstorage.js
@@ -0,0 +1,12 @@
+
+let usuario_html = document.getElementById('usuario');
+
+
+for (let index = 0; index < array.length; index++) {
+    
+    if (usuario_html === ) 
+}
+
+
+
+console.log("¡Hola, mundo desde Node.js!");
